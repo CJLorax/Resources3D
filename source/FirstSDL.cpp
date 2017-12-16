5aa5ae931606407635226ade4a9da9cd713ed9dd
CJLorax <casey.jones@tstc.edu>
2017-12-16 15:03:38 -0600

    added mp3
    added mp3

--- source/FirstSDL.cpp
+++ source/FirstSDL.cpp
@@ -131,7 +131,7 @@ int main(int argc, char* argv[]) {
 
     //set up background audio
 	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 640);
-	Mix_Music *music = Mix_LoadMUS((dirAudio + "Haunted.wav").c_str());
+	Mix_Music *music = Mix_LoadMUS((dirAudio + "Haunted.mp3").c_str());
     Mix_PlayMusic(music, -1);
 
 
