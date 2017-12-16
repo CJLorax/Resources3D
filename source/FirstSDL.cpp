d7aadd4f1b25c2fe3aab1d8349e777f73b5e0b03
CJLorax <casey.jones@tstc.edu>
2017-12-16 15:04:42 -0600

    comment
    comment

--- source/FirstSDL.cpp
+++ source/FirstSDL.cpp
@@ -128,7 +128,7 @@ int main(int argc, char* argv[]) {
 	  exit(1);
 	}
 
-//comment
+//comment 2
     //set up background audio
 	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 640);
 	Mix_Music *music = Mix_LoadMUS((dirAudio + "Haunted.mp3").c_str());
