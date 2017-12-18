6535c67bf60a63883b0f82d02b22fab6d7c03777
CJLorax <casey.jones@tstc.edu>
2017-12-18 14:56:12 -0600

    added some opengl and textures
    added some opengl and textures

--- source/FirstSDL.cpp
+++ source/FirstSDL.cpp
@@ -17,10 +17,14 @@
     // linux
 	#include "glm.hpp"
 	#include "GL/glew.h"
+	#include "GL/glu.h"
 	#include "SDL2/SDL_mixer.h"
 	#include "SDL2/SDL.h"
 	#include "SDL2/SDL_opengl.h"
 	#include <unistd.h>
+
+	#include "shader_s.h"
+	#include "stb_image.h"
 
 
 #elif __APPLE__
@@ -37,27 +41,12 @@ using namespace std;
 #include <iostream>
 
 
-// Shader sources
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
-
 
 // Our SDL_Window ( just like with SDL2 without OpenGL)
 SDL_Window *mainWindow;
 
 // Our opengl context handle
 SDL_GLContext mainContext;
-
 
 
 
@@ -159,6 +148,7 @@ int main(int argc, char* argv[]) {
 	// turn on controller events
 	SDL_GameControllerEventState(SDL_ENABLE);
 
+
 	//SDL event to handle input
 	SDL_Event event;
 
@@ -175,84 +165,106 @@ int main(int argc, char* argv[]) {
 
 
 
-	// build and compile our shader program
-	    // ------------------------------------
-	    // vertex shader
-	    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	    glCompileShader(vertexShader);
-	    // check for shader compile errors
-	    int success;
-	    char infoLog[512];
-	    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	    if (!success)
-	    {
-	        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-	        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	    }
-	    // fragment shader
-	    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	    glCompileShader(fragmentShader);
-	    // check for shader compile errors
-	    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	    if (!success)
-	    {
-	        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-	        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-	    }
-	    // link shaders
-	    int shaderProgram = glCreateProgram();
-	    glAttachShader(shaderProgram, vertexShader);
-	    glAttachShader(shaderProgram, fragmentShader);
-	    glLinkProgram(shaderProgram);
-	    // check for linking errors
-	    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	    if (!success) {
-	        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-	        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-	    }
-	    glDeleteShader(vertexShader);
-	    glDeleteShader(fragmentShader);
+    // build and compile our shader zprogram
+    // ------------------------------------
+    Shader ourShader("4.2.texture.vs", "4.2.texture.fs");
 
-	    // set up vertex data (and buffer(s)) and configure vertex attributes
-	    // ------------------------------------------------------------------
-	    float vertices[] = {
-	        -0.5f, -0.5f, 0.0f, // left
-	         0.5f, -0.5f, 0.0f, // right
-	         0.0f,  0.5f, 0.0f  // top
-	    };
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+    };
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
-	    unsigned int VBO, VAO;
-	    glGenVertexArrays(1, &VAO);
-	    glGenBuffers(1, &VBO);
-	    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-	    glBindVertexArray(VAO);
+    glBindVertexArray(VAO);
 
-	    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-	    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-	    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-	    glBindVertexArray(0);
-
-	    // uncomment this call to draw in wireframe polygons.
-	    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
 
+    // load and create a texture
+    // -------------------------
+    unsigned int texture1, texture2;
+    // texture 1
+    // ---------
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+     // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
+    unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+    // texture 2
+    // ---------
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+    data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
 
-
-
-
-
-
-
+    // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+    // -------------------------------------------------------------------------------------------
+    ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
+    // either set it manually like so:
+    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+    // or set it via the texture class
+    ourShader.setInt("texture2", 1);
 
 
 
@@ -291,19 +303,24 @@ int main(int argc, char* argv[]) {
 
 
 
-		  			// render
-		  			        // ------
-		  			        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		  			        glClear(GL_COLOR_BUFFER_BIT);
 
-		  			        // draw our first triangle
-		  			        glUseProgram(shaderProgram);
-		  			        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		  			        glDrawArrays(GL_TRIANGLES, 0, 3);
-		  			        // glBindVertexArray(0); // no need to unbind it every time
+		  	        // render
+		  	        // ------
+		  	        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		  	        glClear(GL_COLOR_BUFFER_BIT);
 
+		  	        // bind textures on corresponding texture units
+		  	        glActiveTexture(GL_TEXTURE0);
+		  	        glBindTexture(GL_TEXTURE_2D, texture1);
+		  	        glActiveTexture(GL_TEXTURE1);
+		  	        glBindTexture(GL_TEXTURE_2D, texture2);
 
-		  			        SDL_GL_SwapWindow(mainWindow);
+		  	        // render container
+		  	        ourShader.use();
+		  	        glBindVertexArray(VAO);
+		  	        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		  	      SDL_GL_SwapWindow(mainWindow);
 
 
 
@@ -434,7 +451,22 @@ int main(int argc, char* argv[]) {
 		  								}
 
 
-		  						} // button 0 checks IF ends
+		  								/*
+		  									    //**************************** test
+		  									    //transformVertexShaderSource
+		  									    // fragment shader
+		  									    	    int transformVertexShader = glCreateShader(GL_VERTEX_SHADER);
+		  									    	    glShaderSource(transformVertexShader, 1, &transformVertexShaderSource, NULL);
+		  									    	    glCompileShader(transformVertexShader);
+		  									    	    // check for shader compile errors
+		  									    	    glGetShaderiv(transformVertexShader, GL_COMPILE_STATUS, &success);
+		  									    	    if (!success)
+		  									    	    {
+		  									    	        glGetShaderInfoLog(transformVertexShader, 512, NULL, infoLog);
+		  									    	        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+		  									    	    }
+		  									    //**************************** test
+		  								*/		} // button 0 checks IF ends
 		  						break;
 		  				} // end button input checks SWITCH
 		  			} // end SINGLE Poll Event
@@ -489,12 +521,21 @@ int main(int argc, char* argv[]) {
 		  } // end gameState SWITCH
 
 	  } // end game WHILE
-
-
+/*
+	  	//***************************************************** menu screen
 	    // optional: de-allocate all resources once they've outlived their purpose:
 	    // ------------------------------------------------------------------------
 	    glDeleteVertexArrays(1, &VAO);
 	    glDeleteBuffers(1, &VBO);
+	    //***************************************************** menu screen
+*/
+	    glDeleteVertexArrays(1, &VAO);
+	    glDeleteBuffers(1, &VBO);
+	    glDeleteBuffers(1, &EBO);
+
+
+
+
 
 	SDL_GL_DeleteContext(mainContext);
 	Mix_FreeMusic(music);
